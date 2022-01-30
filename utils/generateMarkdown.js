992c8157c0876a08943fcb6ee474817535e527d3
Mariko <mkaku98@gmail.com>
2022-01-29 23:50:59 -0800

    rough draft of markdown layout

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,39 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.name}
+
+  
+  ## Description:
+  ${data.description}
+
+  ## Table Of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [Additional Questions](#additional-questions)
+
+  ## Installation:
+  ${data.installation}
+ 
+  ## Usage:
+  ${data.usage}
+
+  ## License:
+  This repository is licensed under ${data.license}
+
+  ## Contribution:
+  ${data.contribution}
+
+  ## Tests:
+  ${data.test}
+
+  ## For any additional questions:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email}
 
 `;
 }
